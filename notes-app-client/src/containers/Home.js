5898107251d3bc19a5abcf5c91652cc94ff1e6c3
Lucy Copp <lucy.copp@bbc.co.uk>
2019-05-03 19:24:32 +0100

    updates home page

--- notes-app-client/src/containers/Home.js
+++ notes-app-client/src/containers/Home.js
@@ -3,6 +3,8 @@ import "./Home.css";
 import { API } from "aws-amplify";
 import { PageHeader, ListGroup, ListGroupItem } from "react-bootstrap";
 import { LinkContainer } from "react-router-bootstrap";
+import { Link } from "react-router-dom";
+
 
 
 export default class Home extends Component {
@@ -59,9 +61,18 @@ export default class Home extends Component {
       <div className="lander">
         <h1>Scratch</h1>
         <p>A simple note taking app</p>
+        <div>
+          <Link to="/login" className="btn btn-info btn-lg">
+            Login
+          </Link>
+          <Link to="/signup" className="btn btn-success btn-lg">
+            Signup
+          </Link>
+        </div>
       </div>
     );
   }
+  
 
   renderNotes() {
     return (
